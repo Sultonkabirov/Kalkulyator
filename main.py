7c2fbfa9608ecc5a8a53732d894d0010c4c9c70b
Abdulloh <l>
2024-11-01 22:49:41 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+a=int(input("Sonni kiriting:"))
+if a>0 and a<=9:
+    print("Bir xonali")
+elif a>=10 and a<99:
+    print("Ikki xonalik")
+elif a>=100 and a<999:
+    print("Uch xonalik")
+elif a>=1000 and a<=9999:
+    print("Turt xonalik son") 
+elif a>=10000 and a<=99999:
+    print("Besh xonalik son")           
+    
